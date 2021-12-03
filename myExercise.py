72a4d9709f04c0dd9819568130fb78922121a70e
b2200356014 <92159100+b2200356014@users.noreply.github.com>
2021-12-03 14:26:28 +0300

    b2200356014

---
+++ myExercise.py
@@ -0,0 +1,19 @@
+import sys
+
+with open("students.txt", "r+") as file:
+    dic = {}
+    for Iter in file:
+        if Iter[-1] == "\n":
+            dic[Iter.split(":")[0]] = str(Iter[:-1].split(":")[1:])[2:-2].split(",")
+        else:
+            dic[Iter.split(":")[0]] = str(Iter.split(":")[1:])[2:-2].split(",")
+
+    names = sys.argv[1]
+    names = names.split(",")
+    print(names)
+    for name in names:
+        try:
+            print(f"{name},{dic[name][0]},{dic[name][1]}")
+
+        except KeyError:
+            print(f"No record of {name} was found!")
